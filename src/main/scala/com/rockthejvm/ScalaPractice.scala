f77606da4ac3dbfed9638180e9bc3c3d4e567e21
kenneth <kenneth12345645@gmail.com>
2025-08-28 20:36:43 +0800

    Complete exercise 4

--- src/main/scala/com/rockthejvm/ScalaPractice.scala
+++ src/main/scala/com/rockthejvm/ScalaPractice.scala
@@ -24,15 +24,37 @@ object ScalaPractice extends App {
 //    println(multiplyByTwo(4))
 //    println(multiplyByThree(4))
 
-    // create two functions, and use the keyword compose to compose the two functions, use and then as well
+    // Ex3: create two functions, and use the keyword compose to compose the two functions, use and then as well
 
     val func1 = (num:Int) => num + 4
     val func2 = (num:Int) => num * 2
 
     val funcComposed = func1 compose func2 // func1(func2(x))
     val funcAndThen = func2 andThen func1
-    println(funcComposed(3))
-    println(funcAndThen(3))
+//    println(funcComposed(3))
+//    println(funcAndThen(3))
 
+    // Ex4: pattern matching, takes in a string, demarcating operation, and arguments: String
+
+    sealed trait Operation
+    case object ListOp extends Operation
+    case class GrepOp(arguments:String) extends Operation
+    case class RemoveOp(arguments:String) extends Operation
+
+    def patternMatch(op: Operation) = {
+        op match {
+            case ListOp => listFunction()
+            case GrepOp(arguments) => grepFunction(arguments)
+            case RemoveOp(arguments) => removeFunction(arguments)
+        }
+    }
+
+    def listFunction = () => println("Listing all elements")
+    def grepFunction = (matchStr: String) => println(s"Grepping elements that matches with $matchStr")
+    def removeFunction = (path: String) => println(s"Removing files under this path $path")
+
+    patternMatch(ListOp)
+    patternMatch(GrepOp("hello"))
+    patternMatch(RemoveOp("/home"))
 
 }
