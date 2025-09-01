6f2b19ad4f2628b4610b2abfb2f522c0f6a10f64
kenneth <kenneth12345645@gmail.com>
2025-09-01 22:19:21 +0800

    Started on exercise 4

--- src/main/scala/com/rockthejvm/ScalaPractice2.scala
+++ src/main/scala/com/rockthejvm/ScalaPractice2.scala
@@ -1,5 +1,6 @@
 package com.rockthejvm
 
+import java.io.File
 import scala.collection.mutable.ListBuffer
 import scala.math.pow
 import scala.util.Random
@@ -45,12 +46,41 @@ object ScalaPractice2 extends App {
 
     // This version guarantees uniqueness
     val shuffledList = random.shuffle(1 to 49).take(6).sorted
-    println(shuffledList)
+//    println(shuffledList)
 
     // This version does not guarantee uniqueness
     val myList = List.fill(6)(random.between(1,49))
     val newList = myList.sorted
-    println(newList)
+//    println(newList)
+
+    //Ex 4:
+    //Generate a list of the names of all the _files_ in the current directory, excluding the "hidden" files (i.e. those whose names start with ".").
+    //Generate two lists of entries in the directory, one containing the files and one containing the directories.
+    //Generate a list of Pair objects, each containing the name of a file and its size in bytes.
+
+    // TODO: COMPLETE THE REST
+    //Generate a list of the 10 smallest_files_ in the directory, in order, together with their sizes. Do the same for the 10 largest files.
+    //Modify your answer to the above to return a Map where the keys are the names and the values are the sizes Generate
+    // a data structure that arranges the contents of the directory according the the first letter of their name. In other words,
+    // for letter 'a' there should be a list of the entries with names beginning with 'a', and so on...
+
+    val currDir = new File(".")
+    val filesAndDir = currDir.listFiles().toList;
+    val filesInCurrDir = filesAndDir.filter(_.isFile)
+    val fileNames = filesInCurrDir.map(_.getName)
+    val fileSizes = filesInCurrDir.map(_.length())
+    println(filesAndDir)
+    println(filesInCurrDir)
+
+    val filePairs = for {
+        file <- filesInCurrDir
+    } yield (file.getName, file.length())
+    val filePairs2 = fileNames.zip(fileSizes)
+    println(filePairs)
+    println(filePairs2)
+
+
+
 
 
 
