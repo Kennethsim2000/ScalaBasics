e8a5fa08f8e2a3c245e8eb19aa24a12fb0573d5e
kenneth <kenneth12345645@gmail.com>
2025-09-14 12:27:37 +0800

    Complete exercise 4: Recursive func call

--- src/main/scala/com/rockthejvm/ScalaPractice6.scala
+++ src/main/scala/com/rockthejvm/ScalaPractice6.scala
@@ -1,7 +1,9 @@
 package com.rockthejvm
 
+import scala.annotation.tailrec
+
 object ScalaPractice6 extends App {
-    //Implement a function, timed, that allows a caller to measure the time taken to evaluate an expression, which is passed to it as an argument.
+    //Ex3: Implement a function, timed, that allows a caller to measure the time taken to evaluate an expression, which is passed to it as an argument.
     // In other words:
     //val timelExecute = timed do Something}
     //For the moment, assume that the expression is of type Unit.
@@ -21,5 +23,52 @@ object ScalaPractice6 extends App {
     val elapsedTime = timed {
         (1 to 100000).sum
     }
-    println(s"Elapsed time is $elapsedTime")
+
+    // Ex4: Write a recursive function fib(n: Int): Int that uses a recursive algorithm to generate the nth Fibonacci number.
+    // Do you think the return value of Int will be sufficient? How would you change this to make it more useful.
+
+    //Use this function in a further function that generates the first n numbers in the Fibonacci sequence.
+    // Test with the Int return value for your fib function from above, then with your alternative.
+
+    //How many elements of the sequence can you practically generate?
+    //Rewrite the function to generate the Fibonnaci numbers to be tail recursive (verify with the @tailrec annotation).
+    //Use the timed function from earlier to measure the relative performance of the normal recursive and the tail recursive implementation.
+    //See if you can produce a set of data as a sequence of Pairs, (n, time-to-evaluate-fib(n) for each, in order to examine
+    // the effectiveness in terms of scalability of your algorithms.
+
+    //Clue: Create a recursive function for fibonnaci numbers(using Int, then use BigInt), then rewrite it using @tailrec,
+    // note that we have to not have any * x but rather encapsulate it within the parameter.
+    def fib(n:Int): BigInt = {
+        if(n == 1) {
+            1
+        } else {
+            n * fib(n - 1)
+        }
+    }
+
+    @tailrec
+    def fibRec(n: Int, sum: BigInt):BigInt = {
+        if(n == 0) {
+            sum
+        } else {
+            fibRec(n - 1, n * sum)
+        }
+    }
+
+    def fibSeq(num: Int): Seq[BigInt] = {
+        (1 to num).map(x => fib(x))
+    }
+
+    val res = fibSeq(10)
+    println(res)
+
+    val elapsedTime2 = timed {
+        fib(100)
+    }
+    val elapsedTime3 = timed {
+        fibRec(100,1)
+    }
+    println(s"Elapsed time for non tail recursive is $elapsedTime2")
+    println(s"Elapsed time for tail recursive is $elapsedTime3")
+
 }
