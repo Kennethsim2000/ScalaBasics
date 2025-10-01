10d63bffac71c77c399ed7b143e485350978f246
kenneth <kenneth12345645@gmail.com>
2025-10-01 10:51:41 +0800

    Implement binary tree and fold method

--- src/main/scala/com/rockthejvm/ScalaPractice10.scala
+++ src/main/scala/com/rockthejvm/ScalaPractice10.scala
@@ -12,4 +12,19 @@ object ScalaPractice10 extends App {
     //Ex4: Binary tree is a type that can also be defined easily using a recursive ADT: A tree is a leaf node, which contains a value
     //To an internal node, which contains a left node and a right node
     //Implement this type, and provide a fold method for it. Use the fold method to implement length, which returns the number of values (leaf nodes) in the tree
+
+    sealed trait Node {
+        def fold: Int
+    }
+    case class NormalNode(var value: Int, leftNode: Node, rightNode: Node) extends Node {
+
+        override def fold: Int = 1 + leftNode.fold + rightNode.fold
+    }
+    case class LeafNode(var value: Int) extends Node {
+        override def fold: Int = 1
+    }
+
+    val tree = NormalNode(2, NormalNode(3, LeafNode(4), LeafNode(5)), NormalNode(4, LeafNode(6), LeafNode(8)))
+    println(tree.fold)
 }
+
