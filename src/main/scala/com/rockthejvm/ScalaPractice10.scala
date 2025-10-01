755f88e154ce0a95d22115aac987d7c5df2cb92c
kenneth <kenneth12345645@gmail.com>
2025-10-01 10:35:57 +0800

    Implement reverse list using fold left

--- src/main/scala/com/rockthejvm/ScalaPractice10.scala
+++ src/main/scala/com/rockthejvm/ScalaPractice10.scala
@@ -4,6 +4,11 @@ object ScalaPractice10 extends App {
     //Ex3: Look at the documentation for the built-in List (+A) class. Use the foldLeft method to implement reverse on
     // an instance of this class. Can you add this capability to the MyList (A) class?
 
+    val lst = List(1,2,3)
+    val reversedList = lst.foldLeft(List.empty: List[Int])((currList: List[Int], elem: Int) => elem :: currList)
+    val reversedStr = reversedList.mkString(",")
+    println(reversedStr)
+
     //Ex4: Binary tree is a type that can also be defined easily using a recursive ADT: A tree is a leaf node, which contains a value
     //To an internal node, which contains a left node and a right node
     //Implement this type, and provide a fold method for it. Use the fold method to implement length, which returns the number of values (leaf nodes) in the tree
