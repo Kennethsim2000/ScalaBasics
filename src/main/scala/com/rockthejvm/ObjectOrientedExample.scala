dbbff4e7986d21b0396692a8f180173ccaab4b0b
kenneth <kenneth12345645@gmail.com>
2025-10-14 16:11:00 +0800

    Add in fraction implementation

--- src/main/scala/com/rockthejvm/ObjectOrientedExample.scala
+++ src/main/scala/com/rockthejvm/ObjectOrientedExample.scala
@@ -1,6 +1,7 @@
 package com.rockthejvm
 
 import java.time.{LocalDate, Period}
+import javax.swing.event.InternalFrameAdapter
 
 class Trade(val id: String, val symbol: String, val quantity: Int, val initialPrice: Double) {
     require(quantity > 0)  // will prevent the object from being constructed by throwing an illegalArgumentException
@@ -31,6 +32,24 @@ class Person(val dob: LocalDate, val name:String) {
     }
 }
 
+class Fraction private(val numerator: Int, val denominator: Int) {
+    def *(that: Fraction): Fraction = {
+        new Fraction(numerator * that.numerator, denominator * that.denominator)
+    }
+
+    override def toString: String = s"Fraction($numerator/$denominator)"
+}
+
+object Fraction {
+    def apply(numerator:Int, denominator: Int) = {
+        if(denominator != 0) {
+            new Fraction(numerator, denominator)
+        } else {
+            throw new IllegalArgumentException("Denominator cannot be zero")
+        }
+    }
+}
+
 object ObjectOrientedExample extends App {
     val trade = Trade("id", "FB", 100, 10.0)
     trade.price = 20.0
@@ -41,4 +60,10 @@ object ObjectOrientedExample extends App {
     println(trade2)
     val person: Person = Person(LocalDate.of(1990, 5, 15), "kenneth")
     println(person.age)
+
+    val f1 = Fraction(2,3)
+    val f2 = Fraction(3,4)
+    val f3 = f1 * f2
+    println(f3)
+
 }
