9ea55c80e577973f1b41fcd4c5a8bfbe58bb7538
kenneth <kenneth12345645@gmail.com>
2025-10-16 09:23:11 +0800

    Add in flatten example

---
+++ src/main/scala/com/rockthejvm/FlatmapExample.scala
@@ -0,0 +1,8 @@
+package com.rockthejvm
+
+object FlatmapExample extends App {
+    val lst = List("Hello", "World")
+    val lstFlatten = lst.flatten // strings will be flattened to characters
+    println(lstFlatten)
+
+}
